3440a48731ed441b9f5d24b74d1a380abdfbc313
yusufusmahn <yusuf.usman1003@gmail.com>
2025-03-12 16:40:57 +0100

    Update task1.js

--- task1.js
+++ task1.js
@@ -15,22 +15,8 @@ console.log(`${title} by ${author}`);
 
 
 
-/*
-const car = {
-	make: 'Toyota',
-	model: 'Camry',
-	year: 2021
-
-}
 
 
-const getCarProperties = function(car){
 
-	for(let key in car){
-		console.log(car[key]);
-	}
-}
 
-getCarProperties(car); 
 
-*/
